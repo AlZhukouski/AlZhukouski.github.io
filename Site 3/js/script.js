5e09402460e4d0eeacd9f81b43fdbfbc1edb79db
Al.Zhukouski <al.zhukouski@gmail.com>
2019-01-22 18:43:16 +0300

    mistakes

--- Site 3/js/script.js
+++ Site 3/js/script.js
@@ -48,5 +48,17 @@ $(document).ready(function(){
 	$('.container').click(function(event){
 		event.stopPropagation()
 	})
+	$(".slide-animation").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
 
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 800 мс
+		$('body,html').animate({scrollTop: top}, 800);
+	});
 })
