0f54c24c236f053910fca6aef291eb27f0631061
Al.Zhukouski <al.zhukouski@gmail.com>
2019-02-23 11:02:33 +0300

    отправка сообщений на емэйл

---
+++ Site 3/mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$recepient = "yoshi1409@yandex.ru";
+$siteName = "Lacafe";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$message = "Имя: $name \nТелефон: $phone";
+
+$pagetitle = "Заявка с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
